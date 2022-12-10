e5f6d15542c1fbb98b4eeb3746547145cd444807
Dani <daniel.varlan20@gmail.com>
2022-12-10 22:05:36 +0100

    Completed first few lessons

--- 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
+++ 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
@@ -8,6 +8,9 @@
 
 
 // 1. Create two variables, myAge and humanDogRatio
+let myAge = 21
+let humanDogRatio = 7
 // 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = humanDogRatio * myAge
 // 3. Log myDogAge to the console
-
+console.log(myDogAge)

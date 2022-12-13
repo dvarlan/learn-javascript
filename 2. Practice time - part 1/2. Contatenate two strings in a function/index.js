dc15131412658f077b3a1606e7153b2aced9fae9
dvarlan <95964411+dvarlan@users.noreply.github.com>
2022-12-13 12:07:24 +0100

    Finished chapter 2
    Did not check the results with the video but everything is working.

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -1,5 +1,10 @@
-let name = "Linda"
+let namee = "Linda"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function greetingFunc() {
+    console.log(greeting + ", " + namee)
+}
+
+greetingFunc()

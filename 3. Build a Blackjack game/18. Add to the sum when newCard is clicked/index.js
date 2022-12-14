45bd77c4b55aa11635f1127a1efe303fec3bc618
Dani <daniel.varlan20@gmail.com>
2022-12-14 16:39:49 +0100

    Started Working on Chapter 3

--- 3. Build a Blackjack game/18. Add to the sum when newCard is clicked/index.js
+++ 3. Build a Blackjack game/18. Add to the sum when newCard is clicked/index.js
@@ -27,8 +27,9 @@ function startGame() {
 function newCard() {
     console.log("Drawing a new card from the deck!")
     // 1. Create a card variable, and hard code its value to a number (2-11)
-    
+    let newCard = 8
     // 2. Add the new card to the sum variable
-    
+    sum += newCard
     // 3. Call startGame()
+    startGame()
 }
--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,3 +2,10 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("ok")
+} else if (sum === 21) {
+    console.log("Blackjack moment")
+} else {
+    console.log("Bruh")
+}

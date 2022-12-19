a6240e555061e78359422001b7de2ef39623a701
Dani <daniel.varlan20@gmail.com>
2022-12-19 16:27:07 +0100

    Finished more lessons from chapter 5

--- 5. Build a Chrome Extension/22. Clear the input field/index.js
+++ 5. Build a Chrome Extension/22. Clear the input field/index.js
@@ -13,7 +13,9 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        //listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>"
+        console.log(listItems)
     }
     ulEl.innerHTML = listItems  
 }
--- 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
+++ 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
@@ -2,8 +2,10 @@
 
 const recipient = "James"
 // Create a new variable, sender, and set its value to your name
+const sender = "Dani"
+
 
 // Use your sender variable instead of "Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
 
 console.log(email)

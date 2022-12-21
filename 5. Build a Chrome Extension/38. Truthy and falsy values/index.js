9b153365e01c7e8e3493dde018f391be410eae4e
Dani <daniel.varlan20@gmail.com>
2022-12-21 16:49:56 +0100

    Finished more lessons from Chapter 5

--- 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
+++ 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
@@ -1,6 +1,6 @@
-console.log(  Boolean("")   ) //
-console.log(  Boolean("0")  ) //
-console.log(  Boolean(100)  ) //
-console.log(  Boolean(null) ) //
-console.log(  Boolean([0])  ) //
-console.log(  Boolean(-0)   ) //
+console.log(  Boolean("")   ) // False
+console.log(  Boolean("0")  ) // True
+console.log(  Boolean(100)  ) // True
+console.log(  Boolean(null) ) // False
+console.log(  Boolean([0])  ) // True
+console.log(  Boolean(-0)   ) // False
